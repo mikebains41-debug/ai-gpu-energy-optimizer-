cbb86c9b8baea3599a28efa3056a89d20523ed67
mikebains41-debug <mikebains41@gmail.com>
2026-04-18 15:20:24 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/**
+ * PROPRIETARY & CONFIDENTIAL
+ * Copyright (c) 2026 Mike Bains. All Rights Reserved.
+ * Contact: Mikebains41@gmail.com
+ */
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
