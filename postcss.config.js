f0afcb028c18c3052e0c9344894cdda434a0c30d
mikebains41-debug <mikebains41@gmail.com>
2026-04-18 15:22:31 -0700

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+/**
+ * PROPRIETARY & CONFIDENTIAL
+ * Copyright (c) 2026 Mike Bains. All Rights Reserved.
+ * Contact: Mikebains41@gmail.com
+ */
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
