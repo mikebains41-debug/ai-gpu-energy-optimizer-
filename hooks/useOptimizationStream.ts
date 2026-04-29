002c1916549dd20645d29dfbbe44a6c0ef5e6fc8
mikebains41-debug <mikebains41@gmail.com>
2026-04-29 13:00:31 -0700

    Update useOptimizationStream.ts

--- hooks/useOptimizationStream.ts
+++ hooks/useOptimizationStream.ts
@@ -1,3 +1,8 @@
+// PROPRIETARY AND CONFIDENTIAL
+// Copyright (c) 2026 Mike Bains. All Rights Reserved.
+// Contact: Mikebains41@gmail.com
+// Unauthorized use prohibited.
+
 'use client';
 
 import { useState, useEffect, useRef } from 'react';
