2d310e528b0ee2f842319d3ecaaf4a9b96578395
mikebains41-debug <mikebains41@gmail.com>
2026-05-16 15:02:06 -0700

    Remove proprietary core engine, add public reference implementation

--- experiments/workload.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import torch
-print("Running matrix multiplication workload...")
-x = torch.randn(4096, 4096, device='cuda')
-y = torch.randn(4096, 4096, device='cuda')
-for i in range(100):
-    z = torch.matmul(x, y)
-    torch.cuda.synchronize()
-print("Done.")
